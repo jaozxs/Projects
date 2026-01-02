a470e19f8e3e340cf237873f25f97640a81a5c7c
João Siqueira <jfelipe.alves18@gmail.com>
2026-01-01 22:04:11 -0300

    update

---
+++ expense-control/js/script.js
@@ -0,0 +1,18 @@
+// Selecionando os campos de entrada (Inputs e Select)
+const inputDescricao = document.getElementById("input-description");
+const inputValor = document.getElementById("input-value");
+const selectTipo = document.getElementById("select-type");
+
+//  Selecionando o botão de ação
+const btnAdicionar = document.getElementById("inputBtn");
+
+//  Selecionando os elementos de exibição (Lista e Saldo)
+const listaDespesas = document.getElementById("expenses-ul");
+const spanSaldo = document.getElementById("saldo-atual");
+
+//declarando o array de despesas
+const despesas = [];
+
+btnAdicionar.addEventListener("click", (e) => {
+    
+});
